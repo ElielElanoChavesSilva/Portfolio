b575c6241c8070cf9201513734e425c8d50be75e
unknown <elielelano7@gmail.com>
2023-05-01 00:47:00 -0300

    atualizando projetos

--- src/app/views/me/me.component.ts
+++ src/app/views/me/me.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./me.component.css']
 })
 export class MEComponent {
-
+  Down(): void{
+    window.open("https://docs.google.com/document/d/1k_R2KfcUO-L6s5gs4I9gCa9s7xYPsHiM/edit", "_blank")
+  }
 }
--- src/app/views/projects/projects.component.ts
+++ src/app/views/projects/projects.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
+  LinkMine(): void{
+    window.open("https://github.com/ElielElanoChavesSilva/Projetos-Codelandia-_HTML-CSS/tree/main/ProjetoMINE", "blank");
+  }
+  
+  LinkLoki(): void{
+    window.open("https://github.com/ElielElanoChavesSilva/Projetos-Codelandia-_HTML-CSS/tree/main/ProjetoLoki", "blank");
+  }
+  
+  LinkImc(): void{
+    window.open("https://github.com/ElielElanoChavesSilva/Projetos-Codelandia-_HTML-CSS/tree/main/Calculadora-IMC", "blank");
+  }
 
+  LinkCrud(): void{
+    window.open("https://github.com/ElielElanoChavesSilva/Angular", "blank");
+  }
 }
